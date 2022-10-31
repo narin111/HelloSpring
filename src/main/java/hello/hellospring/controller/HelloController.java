3a2c2ab60da7b1da1e8f4dbfe99bf4e2eb5b6014
narin111 <jungnr11@naver.com>
2022-10-31 22:45:11 +0900

    addAPI

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -21,6 +22,30 @@ public class HelloController {
         return "hello-template";
     }
 
+    @GetMapping("hello-string")
+    @ResponseBody // http의 body(:통신 프로토콜 구조) 부분에 직접 넣어주겠다
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name; // 링크에서 적은게 그대로 내려감
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
 
 // ctrl 누르고 return 값 누르면 연결된 곳 바로감
