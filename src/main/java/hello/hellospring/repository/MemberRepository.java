0f4ce6456cf98c0499a9075f57f3ead09b503685
narin111 <jungnr11@naver.com>
2022-11-02 01:48:24 +0900

    addMemberBusiness 1. create Member Class 2. add MemberResitory&Interface 3. save, find Member

---
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member); // 회원이 저장소에 저장
+    Optional<Member> findById(Long id); // 자바8에 포함된 기능 findbyid가 null일 경우 그대로 반환하기보다
+    Optional<Member> findByName(String name);
+    List<Member> findAll(); // 저장된 모든 회원리스트 전달
+
+
+}
---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,33 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;// 0 1 2 키 값 생성 - 동시성 문제 고려x
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member); // 해시맵에 저장
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); // null을 반환할 경우 optional으로 감싼다.
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny(); // 하나 찾으면 반환해버림
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());// 실무에서는 List 많이씀
+    }
+}
