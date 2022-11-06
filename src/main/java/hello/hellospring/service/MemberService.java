ee627f33a3689b09de1b3a0cb92c02d65ed857f0
narin111 <jungnr11@naver.com>
2022-11-06 15:56:43 +0900

    스프링빈 등록

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -3,13 +3,17 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+@Service
 public class MemberService { // ctrl shift t => new test
     // private final MemberRepository memberRepository = new MemoryMemberRepository();
+
     private final MemberRepository memberRepository;
+    @Autowired
     public MemberService(MemberRepository memberRepository){ // 외부에서 memberRepository 넣어줌 -> dependency infection
         this.memberRepository = memberRepository;
     }
