505fa7229121f9f9e797ad1b74fb968d84d3f5ba
narin111 <jungnr11@naver.com>
2022-11-06 17:40:21 +0900

    회원가입+회원목록

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,9 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -19,4 +24,27 @@ public class MemberController {
     public String createForm(){
         return "members/createMemberForm";
     }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName()) ;
+
+        memberService.join(member); // join -> save
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        /* memberList.html
+            <tr th:each="member : ${members}">
+            <td th:text="${member.id}"></td>
+            <td th:text="${member.name}"></td>
+            </tr>
+         */
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
 }
