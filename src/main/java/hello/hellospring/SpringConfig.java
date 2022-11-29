a895b17ea5d13afa7312adc0edbb73cbca9525fd
narin111 <jungnr11@naver.com>
2022-11-30 00:41:29 +0900

    feat: 데이터베이스 등록

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,22 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    // spring boot가 자체적으로 빈 생성, 데이터베이스를 연결할 수 있는 정보를 만들어줌
+    private DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -15,6 +24,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource); // 스프링을 사용하는 결정적인 이유
     }
 }
